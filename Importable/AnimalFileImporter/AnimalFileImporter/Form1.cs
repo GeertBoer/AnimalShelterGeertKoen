5048881e7489e6db0cfcec4a15adce23b204f1e7
KVerhofstadt <floxsta@gmail.com>
2016-10-17 17:32:54 +0200

    Animalfileimportable + map

---
+++ Importable/AnimalFileImporter/AnimalFileImporter/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimalFileImporter
+{
+    public partial class AnimalFileImporter : Form
+    {
+        public AnimalFileImporter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            List<string> lines = new List<string>();
+            if(ofdGetAnimals.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = ofdGetAnimals.FileName;
+                using (StreamReader stream = new StreamReader(fileName))
+                {
+                    string line;
+                    while((line = stream.ReadLine()) != null)
+                    {
+                        lbImportedAnimals.Items.Add(line);
+                    }
+                    
+                }
+            }
+        }
+    }
+}
